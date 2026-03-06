570bbf7ef0bfcfa006a00d53425f8756c98fc97e
GuiLuiz06 <margilepow1227@gmail.com>
2026-03-06 16:56:42 -0300

    iniciei a aba sobre mim

--- portfolio/src/components/contatos/Contats.jsx
+++ portfolio/src/components/contatos/Contats.jsx
@@ -5,7 +5,7 @@ function Contact() {
     return (
         <>
             <div className="flex flex-col gap-3">
-                <ContactButton nome={"Linkedin"} link={"https://linkedin.com/guiilhermeluiz"} Icon={Linkedin} />
+                <ContactButton nome={"Linkedin"} link={"https://linkedin.com/in/guiilhermeluiz"} Icon={Linkedin} />
                 <ContactButton nome={"GitHub"} link={"https://GitHub.com/GuiLuiz06"} Icon={Github} />
                 <ContactButton nome={"Email"} link={"https://linkedin.com/guiilhermeluiz"} Icon={Mail} />
             </div>
